ca4731212bbbff95bfcc8bac660c6dc5885f5f7e
Risfat97 <tafsirmndiour@tafsir-ndiour.local>
2021-12-20 00:30:51 +0100

    Jant TECH v2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,93 @@
 (function(){
-    var closeDropdown = (btnDropdown) => {
+    const closeDropdown = (btnDropdown) => {
         let iconeSpan = btnDropdown.firstElementChild;
         iconeSpan.textContent = "menu";
         let dropdown = document.querySelector('.dropdown-container');
         dropdown.classList.remove('d-block');
         dropdown.classList.add('d-none');
-    }
+    };
 
-    var openDropdown = (btnDropdown) => {
+    const openDropdown = (btnDropdown) => {
         let iconeSpan = btnDropdown.firstElementChild;
         iconeSpan.textContent = "close";
         let dropdown = document.querySelector('.dropdown-container');
         dropdown.classList.remove('d-none');
         dropdown.classList.add('d-block');
+    };
+
+    const showError = (domElement) => {
+        if(domElement.classList.length === 4 && domElement.classList[3] === 'd-none')
+            domElement.classList.remove('d-none');
     }
 
+    const hideError = (domElement) => {
+        if(domElement.classList.length !== 4)
+            domElement.classList.add('d-none');
+    }
+
+    const checkFormContact = () => {
+        if(inputName.value.length < 2){
+            showError(errName);
+            return false;
+        }
+        else
+            hideError(errName);
+
+        let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!pattern.test(inputMail.value)){
+            showError(errMail);
+            return false;
+        }
+        else 
+            hideError(errMail);
+
+        if(inputMsg.value.length < 1){
+            showError(errMsg);
+            return false;
+        }
+        else
+            hideError(errMsg);
+
+        return true;
+    };
+
+    const sendMail = () => {
+        let name = inputName.value.trim(),
+            mail = inputMail.value.trim(),
+            msg = inputMsg.value.trim();
+        fetch('/mailer.php', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name, 
+                mail: mail,
+                msg: msg
+            })
+        })
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+        })
+        .catch(function (err){
+            console.log(err);
+        });
+
+        inputName.value = '';
+        inputMail.value = '';
+        inputMsg.value = '';
+    };
+
     var btnMenu = document.querySelector('a.btn-menu');
+    var inputName = document.querySelector('input[name="name"'),
+        errName = document.querySelector('small.err-name'),
+        inputMail = document.querySelector('input[name="mail"'),
+        errMail = document.querySelector('small.err-mail'),
+        inputMsg = document.querySelector('textarea[name="msg"'),
+        errMsg = document.querySelector('small.err-msg'),
+        formContact = document.getElementById('formContact');
     
     if(btnMenu){
         let iconeSpan = btnMenu.firstElementChild;
@@ -34,4 +106,10 @@
             });
         });
     }
+
+    formContact.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if(checkFormContact())
+            sendMail();
+    })
 })();
